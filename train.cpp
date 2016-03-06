20c4fafb69a288779baf533aa4b75fa0589e92c5
nosyarg <york17a@ncssm.edu>
2016-03-06 16:28:06 -0500

    added libraries and train program skeleton

---
+++ train.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include "armadillo-6.500.5/include/armadillo"
+using namespace std;
+int main()
+{
+cout << "hello world" << endl;
+return 0;
+}
